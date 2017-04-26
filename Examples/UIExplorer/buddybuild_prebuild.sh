539a5dc6e9ccd43650a048f316f19da1b95d729d
Christopher Stott <christopherstott@gmail.com>
2017-04-25 21:27:39 -0700

    Launch websocket server

--- Examples/UIExplorer/buddybuild_prebuild.sh
+++ Examples/UIExplorer/buddybuild_prebuild.sh
@@ -2,6 +2,9 @@
 
 cd $BUDDYBUILD_WORKSPACE
 pwd
-open "./packager/launchPackager.command" || echo "Can't start packager automatically"
-open "./IntegrationTests/launchWebSocketServer.command" || echo "Can't start web socket server automatically"
+#open "./packager/launchPackager.command" || echo "Can't start packager automatically"
+#open "./IntegrationTests/launchWebSocketServer.command" || echo "Can't start web socket server automatically"
+
+cd IntegrationTests
+node websocket_integration_test_server.js &
 
