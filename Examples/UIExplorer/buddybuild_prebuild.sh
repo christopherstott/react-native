eec8953fdb06e9eadffd4a49b58bc3305c22450a
Christopher Stott <christopherstott@gmail.com>
2017-04-25 21:34:51 -0700

    Launch websocket server 2

--- Examples/UIExplorer/buddybuild_prebuild.sh
+++ Examples/UIExplorer/buddybuild_prebuild.sh
@@ -6,5 +6,5 @@ pwd
 #open "./IntegrationTests/launchWebSocketServer.command" || echo "Can't start web socket server automatically"
 
 cd IntegrationTests
-node websocket_integration_test_server.js &
+./websocket_integration_test_server.js &
 
